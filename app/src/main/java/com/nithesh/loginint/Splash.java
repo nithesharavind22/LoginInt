521c113592cd90dc5f4ea9685320947922b308d1
nithesharavind22 <nithesharavind22@gmail.com>
2021-06-16 19:59:26 +0530

    Initial Version

---
+++ app/src/main/java/com/nithesh/loginint/Splash.java
@@ -0,0 +1,53 @@
+package com.nithesh.loginint;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends Activity {
+
+    TextView designed, name, app_name;
+    ImageView logo, imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.logo);
+        designed = findViewById(R.id.designed);
+        name = findViewById(R.id.name);
+        app_name = findViewById(R.id.app_name);
+        imageView2 = findViewById(R.id.imageView2);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startEnterAnimation();
+            }
+        }, 2000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+
+            }
+        }, 5500);
+    }
+
+    private void startEnterAnimation() {
+        logo.setVisibility(View.VISIBLE);
+        designed.setVisibility(View.VISIBLE);
+        name.setVisibility(View.VISIBLE);
+        app_name.setVisibility(View.VISIBLE);
+        imageView2.setVisibility(View.VISIBLE);
+    }
+}
